9eba1fea266b61552f4c67ea2b1df40ccde802a9
Junho Choi <tig9659@ajou.ac.kr>
2023-08-13 03:37:32 +0900

    모든 제품 정보 페이지

--- mysite/views.py
+++ mysite/views.py
@@ -5,3 +5,8 @@ def index(req):
     content_list = Product.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(req, 'mysite/content_list.html', context)
+
+def products(req):
+    content_list = Product.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(req, 'mysite/products_info.html', context)
