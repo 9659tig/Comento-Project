cb15ee34be5be3b44af988e22957c3d8de01c2d1
Junho Choi <tig9659@ajou.ac.kr>
2023-08-15 11:00:36 +0900

    이미지 처리

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,7 +71,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'my_project.wsgi.application'
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
