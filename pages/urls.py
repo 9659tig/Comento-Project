999549ff1d74699eefd1febc915c5cf6ca65784b
Junho Choi <tig9659@ajou.ac.kr>
2023-08-16 23:27:35 +0900

    로그인, 로그아웃, 회원가입 기능구현

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from .import views
+from mysite.views import detail
 
 urlpatterns = [
     path('', views.mainpage),
     path('company/', views.company),
+    path('<int:content_id>/', detail, name='detail'),
 ]
