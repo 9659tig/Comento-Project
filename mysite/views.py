c9fbbd2e7b662dd47fbed190007ea1c4cfdce16b
Junho Choi <tig9659@ajou.ac.kr>
2023-08-23 01:11:13 +0900

    댓글 수정, 삭제

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import  login_required
-from .models import Product
+from .models import Product, Comment
 from .forms import CommentForm
 
 def index(req):
@@ -35,3 +36,32 @@ def comment_create(req, content_id):
             form = CommentForm()
             context = {'content_list':content_list, 'form':form}
             return render(req, 'mysite/content_detail.html', context)
+
+@login_required(login_url='accounts:login')
+def comment_update(req, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if req.user != comment.author:
+        raise PermissionDenied
+
+    if req.method == 'POST':
+        form = CommentForm(req.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('detail', content_id=comment.content_list.id)
+    else:
+        form = CommentForm(instance=comment)
+
+    context = {'comment': comment, 'form': form}
+    return render(req, 'mysite/comment_form.html', context)
+
+@login_required(login_url='accounts:login')
+def comment_delete(req, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if req.user != comment.author:
+        raise PermissionDenied
+    else:
+        comment.delete()
+    return redirect('detail', content_id=comment.content_list.id)
